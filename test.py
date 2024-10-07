84581bc28d5b6fed96234059fb0c1370677447bb
piadozo-edriane <doidoiwey@gmail.com>
2024-10-07 10:41:08 +0800

    save changes in my file

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+import os
+
+os.system ('cls')
 print ("Hi my name is Edriane")
 print ("I like to try this and check what will happen in my playground")
+print ("The quick brown fox jumps over the lazy dog")
