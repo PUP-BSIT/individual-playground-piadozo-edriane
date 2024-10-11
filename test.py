7f9796e21072ec3f5a7114f1c083a952e11cc56f
piadozo-edriane <doidoiwey@gmail.com>
2024-10-12 05:30:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ os.system ('cls')
 print ("Hi my name is Edriane")
 print ("I like to try this and check what will happen in my playground")
 print ("The quick brown fox jumps over the lazy dog")
+
+print ("Goodluck for your day")
