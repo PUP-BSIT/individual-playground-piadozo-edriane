3c96edf8afa9babb77fda3f18daba8361e6bb110
piadozo-edriane <doidoiwey@gmail.com>
2024-12-16 20:22:04 +0800

    Upload my module in exercise 15

--- practice.py
+++ practice.py
@@ -2,6 +2,8 @@ import os
 from math import sqrt
 import re
 import operator
+import random
+import webbrowser
 
 #KEEP GOING, AND PRACTICE ANY PROBLEM YOU WANT !
 """
@@ -1188,6 +1190,7 @@ while user_input.isnumeric():
 
 print ("Average: ", calculate_average(numbers))
 """
+""" 
 dict_word ={}
 def occur_of_letter (input_word):
     for num in input_word:
@@ -1261,9 +1264,260 @@ match choice:
 
 
 def format_name(f_name, l_name):
-  """Take a first and last name and format it to title case."""
+  Take a first and last name and format it to title case.
   if f_name == "" or l_name == "":
       return "You didn't provide valid inputs."
   formatted_f_name = f_name.title()
   formatted_l_name = l_name.title()
   return f"{formatted_f_name} {formatted_l_name}"
+"""
+""" 
+def edriane():
+    while True:
+        os.system('cls')
+        print ("1. Who am I ?")
+        print ("2. What's your thought to me as a teammate :>")
+        print ("3. Rate my function")
+        print ("4. Exit")
+        user_choice = int(input("Enter your choice of number: "))
+        if user_choice == 4:
+            print ("Thanks B)")
+            break
+
+        match user_choice:
+            case 1:
+                while True:
+                    os.system("cls")
+                    print ("I am Edriane")
+                    print ("a. Birthday          b"". Hobby")
+                    print ("c. Favorite movie    d"". Favorite Series")
+                    print ("e. Word of Wisdom    f"". Dream")
+                    print ("g. Exit")
+                    know_me = input("Enter your choice: ")
+                    if know_me == "g":
+                         print ("Thank you B)")
+                         break
+                        
+                    match know_me:
+                        case "a":
+                                os.system('cls')
+                                print ("My birthday is January 1 2005")
+                                present = str (input("Do you have a gift for me Y/N ?: "))
+
+                                if present == 'Y':
+                                    print ("Ahhh thank you :*")
+                                    input()
+                                elif present == "N":
+                                    print ("NOOO")
+                                    input()
+                                    
+                        case "b":
+                                os.system('cls')
+                                print ("My hobby is playing online games")
+                                input("Press enter to continue")
+                        case "c":
+                                os.system('cls')
+                                print ("My favorite movie is Whiplash")
+                                input("Press enter to continue")
+                        case "d":
+                                os.system('cls')
+                                print ("My favorite series is Breaking bad and Better Call Saul")
+                                input("Press enter to continue")
+                        case "e":
+                                os.system('cls')
+                                print ("Let it happen")
+                                input("Press enter to continue")
+                        case "f":
+                            os.system('cls')
+                            print ("Become a Game Developer")
+                            print ("Develop a game that will remember me")
+                            input("Press enter to continue")
+                        case _:
+                            os.system('cls')
+                            print ("Invalid choice")
+            case 2:
+                os.system('cls')
+                why_o_why = ["Am I not good teammate ?", "Enter again >:(", "Why not press Y ?","Why press N 8-< ?"]
+                while True:
+                    choice_team = str(input("Am I a good team mate Y/N ?: "))
+                    random_comment = random.randint(0,3)
+
+                    if choice_team == "N":
+                        print (why_o_why[random_comment])
+                        input()
+                    elif choice_team == "Y":
+                        print ("Thank you B)")
+                        input()
+                        break
+            case 3:
+                  os.system('cls')
+                  rate_my_module = int(input("Enter your rate 1-10: "))
+                  if rate_my_module == 10:
+                       print ("Thanks B)")
+                       input()
+                  elif rate_my_module >= 5 and rate_my_module <= 9:
+                       print ("Why :(")
+                       input()
+                  elif rate_my_module >= 0 and rate_my_module <= 4:
+                       print ("k")
+                       input ()
+            case _:
+                print ("Invalid choice")
+
+edriane()
+"""
+unset_option = -1
+exit_option = 0
+
+class Cardgame:
+    list_of_account = []
+    def __init__(self, name, pin, age, balance_user):
+        self.name = name
+        self.pin = pin
+        self.age = age
+        self.balance_user = balance_user
+
+    def create_account (self):
+        num_account = int(input("Enter the number of account: "))
+        for num in range (num_account):
+            print(f"Account number {num}")
+            user_name = str(input("Enter your username: "))
+            user_pin = str(input("Enter your user pin: "))
+            user_age = int(input("Enter your age: "))
+            user_balance = 0
+            account = Cardgame(user_name, user_pin, user_age, user_balance)
+            Cardgame.list_of_account.append(account)
+
+    def user_information(self):
+            print (f"Your name is: {self.name}")
+            print (f"Your user pin is: {self.pin}")
+            print (f"Your age is: {self.age}")
+            print (f"Your current balance is: {self.balance_user}")
+    
+    def user_information_display ():
+        account_finder = int(input("Enter a number to find your account "
+                                   + "start from 0: "))
+        if Cardgame.list_of_account:
+            Cardgame.list_of_account[account_finder].user_information()
+        else:
+            print ("No accounts found")
+
+    def registerd_account ():
+        print ("List of account")
+        if Cardgame.list_of_account:
+            for account in Cardgame.list_of_account:
+                account.user_information()
+                print ("-" * 30)
+        else:
+            print ("No accounts")
+
+    def input_balance (self):
+        if self.age >= 18 and self.balance_user <= 0:
+            print ("Please input a balance")
+            user_input_balance = int(input("Top up : "))
+            self.balance_user += user_input_balance
+            print (f"Your total balance is: {self.balance_user}")
+        elif self.age <= 17:
+            print ("Age restricted")
+            return
+    
+    def input_balance_display ():
+        choce_account = int(input("Enter your number of account: "))
+        if Cardgame.list_of_account:
+            Cardgame.list_of_account[choce_account].input_balance()
+        else:
+            print("No accound found")
+
+    def card_game (self):
+        if self.balance_user > 0:
+            print ("Card game Baccarat")
+            choice = input("Y to continue N to return to main menu: ")
+
+            if choice == "Y":
+                place_bet = input("Placebet (Banker/Player): ")
+                money_bet = int(input("Enter your bet: "))
+                card_one = random.randint(1, 9)
+                card_two = random.randint(1,9)
+                card_three = random.randint(1,9)
+                card_four = random.randint(1,9)
+                result_banker = (card_one + card_two) % 10
+                result_player = (card_three + card_four) % 10
+
+                print (f"The result of banker: {result_banker}")
+                print (f"The result of player: {result_player}")
+
+                if place_bet == "Banker" and result_banker > result_player:
+                    print ("Congratulations you win")
+                    self.balance_user += money_bet
+                elif result_banker < result_player:
+                    print ("Sorry you lose")
+                    self.balance_user -= money_bet
+                elif result_player == result_banker:
+                    print ("It's a draw")
+                    self.balance_user = self.balance_user
+
+                if place_bet == "Player" and result_player > result_banker:
+                    print ("Congratulations you win")
+                    self.balance_user += money_bet
+                elif result_player < result_banker:
+                    print ("Sorry you lose")
+                    self.balance_user -= money_bet
+                elif result_player == result_banker:
+                    print ("It's a draw")
+                    self.balance_user = self.balance_user
+        else:
+            print ("Thank you")
+            pass
+
+    def card_game_display ():
+        choce_account = int(input("Enter your number of account: "))
+        if Cardgame.list_of_account:
+            Cardgame.list_of_account[choce_account].card_game()
+        else:
+            print("No accound found")
+            
+game = Cardgame("", "", 0, 0)
+
+def piadozo ():
+    os.system("cls")
+    main_choice = unset_option
+    while main_choice != exit_option:
+        main_choice = display_choice()
+        process_choice(main_choice)
+
+def display_choice ():
+    os.system("cls")
+    print ("1. Create account")
+    print ("2. Information")
+    print ("3. Registered account")
+    print ("4. Input a balance")
+    print ("5. Card game")
+
+    return int(input("Enter your choice: "))
+
+def process_choice (main_choice):
+    match main_choice:
+        case 1:
+            os.system("cls")
+            game.create_account()
+            input()
+        case 2:
+            os.system("cls")
+            Cardgame.user_information_display()
+            input()
+        case 3:
+            os.system("cls")
+            Cardgame.registerd_account()
+            input()
+        case 4:
+            os.system("cls")
+            Cardgame.input_balance_display()
+            input()
+        case 5:
+            os.system("cls")
+            Cardgame.card_game_display()
+            input()
+        case _:
+            print ("Invalid")
+
+piadozo()
